3722e117d44137ed311bb52f9628f4341604f73c
DimaPidko <pidkodima@gmail.com>
2024-01-14 14:26:30 +0200

    screen 3,4. Script for skills %

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
-const 
-    hamburger = document.querySelector(".hamburger"),
-    menu = document.querySelector(".menu"),
-    close = document.querySelector(".menu__close");
+const hamburger = document.querySelector('.hamburger'),
+	menu = document.querySelector('.menu'),
+	close = document.querySelector('.menu__close'),
+	counters = document.querySelectorAll('.skills__ratings-counter'),
+	lines = document.querySelectorAll('.skills__ratings-line span');
+
 
 hamburger.addEventListener("click", () => {
     menu.classList.add("active");
 });
 
 close.addEventListener('click', () => {
-	menu.classList.remove('active')
-})
+	menu.classList.remove('active');
+});
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
